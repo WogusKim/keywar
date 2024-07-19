2cc592dcc521b7a54a43b898f5fec714de73ff62
cse <xingen7@naver.com>
2024-07-19 14:17:16 +0900

    웹소켓 미지원으로 인해 제거함.

--- Keyboard/src/main/java/kb/keyboard/warrior/util/WebSocketConfig.java
+++ Keyboard/src/main/java/kb/keyboard/warrior/util/WebSocketConfig.java
@@ -1,21 +1,24 @@
 package kb.keyboard.warrior.util;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
-
-@Configuration
-@EnableWebMvc
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-	@Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new NotificationWebSocketHandler(), "/notifications")
-                .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .setAllowedOrigins("*");
-    }
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+//
+//@Configuration
+//@EnableWebMvc
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer {
+//	@Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(new NotificationWebSocketHandler(), "/notifications")
+//                .addInterceptors(new HttpSessionHandshakeInterceptor())
+//                .setAllowedOrigins("*");
+//    }
+//}
+public class WebSocketConfig{
+	
 }
