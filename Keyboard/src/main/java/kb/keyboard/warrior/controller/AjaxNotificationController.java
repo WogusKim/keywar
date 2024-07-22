1949ecd18a561a88ce7a675e467c605b37d0e25b
cse <xingen7@naver.com>
2024-07-22 17:17:16 +0900

    json데이터 한글깨짐 문제 해결

--- Keyboard/src/main/java/kb/keyboard/warrior/controller/AjaxNotificationController.java
+++ Keyboard/src/main/java/kb/keyboard/warrior/controller/AjaxNotificationController.java
@@ -1,18 +1,24 @@
 package kb.keyboard.warrior.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+
+
 import kb.keyboard.warrior.dao.*;
+import kb.keyboard.warrior.dto.AlertDTO;
 
 @Controller
 public class AjaxNotificationController {
@@ -20,27 +26,47 @@ public class AjaxNotificationController {
 	@Autowired
 	public SqlSession sqlSession;
 
-    @RequestMapping(value = "/ajaxNotification", method = RequestMethod.GET)
+
+//    @Autowired
+//    private ObjectMapper objectMapper; // 주입된 ObjectMapper 사용
+	
+	
+	@RequestMapping(value = "/ajaxNotification", method = RequestMethod.GET)
     @ResponseBody
-    public String getNotifications(@RequestParam(value = "userno", required = false) String userno) {
+    public void getNotifications(HttpServletResponse response) throws Exception {
         // HttpSession을 가져옵니다.
+    	
+    	String userno ="";
         ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpSession session = attrs.getRequest().getSession(false);
         if (session != null) {
             userno = (String) session.getAttribute("userno");
+            System.out.println(userno);
         }
         
         // 알림 데이터 처리
         String data = getData(userno);
         
-        return "{\"message\": \"" + (data != null ? data : "No new notifications") + "\"}";
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(data);
     }
 
-    private String getData(String userno) {
+    private String getData(String userno) throws  Exception {
     	AlertDao dao = sqlSession.getMapper(AlertDao.class);
+    	List<AlertDTO> list = dao.getAlert(userno);
+    	
+    	if(list==null) {
+    		 return "{\"message\": \"No new notifications\"}";
+    	}
+    	System.out.println(userno +"의 안읽은 알림 개수 : "+list.size());
+    	System.out.println(list.get(0).getMessage());
     	
     	
     	
-        return "New notification !"; // 또는 null
+    	// ObjectMapper를 사용하여 JSON 직렬화
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(list);
+    	
+        return json;
     }
 }
