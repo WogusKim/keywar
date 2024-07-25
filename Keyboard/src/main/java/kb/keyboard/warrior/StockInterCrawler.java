1e0d3c4135bbda77ea0879aa082238a2f03eef5f
moonjaewook <jumoon92@gmail.com>
2024-07-25 09:42:10 +0900

    한글 깨짐 해결

--- Keyboard/src/main/java/kb/keyboard/warrior/StockInterCrawler.java
+++ Keyboard/src/main/java/kb/keyboard/warrior/StockInterCrawler.java
@@ -11,15 +11,15 @@ import org.jsoup.nodes.Element;
 import kb.keyboard.warrior.dto.StockDTO;
 
 public class StockInterCrawler {
-	
-//	public static void main(String[] args) {
+    
+//    public static void main(String[] args) {
 //        StockInterCrawler crawler = new StockInterCrawler();
 //        List<StockDTO> stocks = crawler.fetchIndexData();
 //        for (StockDTO stock : stocks) {
 //            System.out.println(stock);
 //        }
 //    }
-//	
+//    
 //    public static void main(String[] args) {
 //        StockInterCrawler crawler = new StockInterCrawler();
 //        List<String> urls = new ArrayList<String>();
@@ -35,58 +35,58 @@ public class StockInterCrawler {
 //        crawler.fetchIndexData(urls);
 //    }
 
-	public List<StockDTO> fetchIndexData() {
+    public List<StockDTO> fetchIndexData() {
         List<StockDTO> stockList = new ArrayList<StockDTO>();
         String[] urls = {
-        		"https://finance.naver.com/world/sise.naver?symbol=DJI@DJI",
-        		"https://finance.naver.com/world/sise.naver?symbol=DJI@DJT",
-        		"https://finance.naver.com/world/sise.naver?symbol=NAS@IXIC",
-        		"https://finance.naver.com/world/sise.naver?symbol=NAS@NDX",
-        		"https://finance.naver.com/world/sise.naver?symbol=SPI@SPX",
-        		"https://finance.naver.com/world/sise.naver?symbol=NAS@SOX",
-        		"https://finance.naver.com/world/sise.naver?symbol=BRI@BVSP",
-        		"https://finance.naver.com/world/sise.naver?symbol=SHS@000001",
-        		"https://finance.naver.com/world/sise.naver?symbol=SHS@000002",
-        		"https://finance.naver.com/world/sise.naver?symbol=SHS@000003",
-        		"https://finance.naver.com/world/sise.naver?symbol=NII@NI225",
-        		"https://finance.naver.com/world/sise.naver?symbol=HSI@HSI",
-        		"https://finance.naver.com/world/sise.naver?symbol=HSI@HSCE",
-        		"https://finance.naver.com/world/sise.naver?symbol=HSI@HSCC",
-        		"https://finance.naver.com/world/sise.naver?symbol=TWS@TI01",
-        		"https://finance.naver.com/world/sise.naver?symbol=INI@BSE30",
-        		"https://finance.naver.com/world/sise.naver?symbol=MYI@KLSE",
-        		"https://finance.naver.com/world/sise.naver?symbol=IDI@JKSE",
-        		"https://finance.naver.com/world/sise.naver?symbol=LNS@FTSE100",
-        		"https://finance.naver.com/world/sise.naver?symbol=PAS@CAC40",
-        		"https://finance.naver.com/world/sise.naver?symbol=XTR@DAX30",
-        		"https://finance.naver.com/world/sise.naver?symbol=STX@SX5E",
-        		"https://finance.naver.com/world/sise.naver?symbol=RUI@RTSI",
-        		"https://finance.naver.com/world/sise.naver?symbol=ITI@FTSEMIB"
+                "https://finance.naver.com/world/sise.naver?symbol=DJI@DJI",
+                "https://finance.naver.com/world/sise.naver?symbol=DJI@DJT",
+                "https://finance.naver.com/world/sise.naver?symbol=NAS@IXIC",
+                "https://finance.naver.com/world/sise.naver?symbol=NAS@NDX",
+                "https://finance.naver.com/world/sise.naver?symbol=SPI@SPX",
+                "https://finance.naver.com/world/sise.naver?symbol=NAS@SOX",
+                "https://finance.naver.com/world/sise.naver?symbol=BRI@BVSP",
+                "https://finance.naver.com/world/sise.naver?symbol=SHS@000001",
+                "https://finance.naver.com/world/sise.naver?symbol=SHS@000002",
+                "https://finance.naver.com/world/sise.naver?symbol=SHS@000003",
+                "https://finance.naver.com/world/sise.naver?symbol=NII@NI225",
+                "https://finance.naver.com/world/sise.naver?symbol=HSI@HSI",
+                "https://finance.naver.com/world/sise.naver?symbol=HSI@HSCE",
+                "https://finance.naver.com/world/sise.naver?symbol=HSI@HSCC",
+                "https://finance.naver.com/world/sise.naver?symbol=TWS@TI01",
+                "https://finance.naver.com/world/sise.naver?symbol=INI@BSE30",
+                "https://finance.naver.com/world/sise.naver?symbol=MYI@KLSE",
+                "https://finance.naver.com/world/sise.naver?symbol=IDI@JKSE",
+                "https://finance.naver.com/world/sise.naver?symbol=LNS@FTSE100",
+                "https://finance.naver.com/world/sise.naver?symbol=PAS@CAC40",
+                "https://finance.naver.com/world/sise.naver?symbol=XTR@DAX30",
+                "https://finance.naver.com/world/sise.naver?symbol=STX@SX5E",
+                "https://finance.naver.com/world/sise.naver?symbol=RUI@RTSI",
+                "https://finance.naver.com/world/sise.naver?symbol=ITI@FTSEMIB"
         };
 
         for (String url : urls) {
             try {
                 Document doc = Jsoup.connect(url).get();
 
-                // Index name Í∞ÄÏ†∏Ïò§Í∏∞
+                // Index name ∞°¡Æø¿±‚
                 Element countryElement = doc.selectFirst(".h_area .state");
                 String country = countryElement != null ? countryElement.text() : "Unknown Index";
                 
                 Element indexNameElement = doc.selectFirst(".h_area h2");
                 String indexName = indexNameElement != null ? indexNameElement.text() : "Unknown Index";
 
-                // ÌòÑÏû¨ ÏßÄÏàò Í∞ÄÏ†∏Ïò§Í∏∞
+                // «ˆ¿Á ¡ˆºˆ ∞°¡Æø¿±‚
                 Element todayElement = doc.selectFirst(".no_today");
                 String currentPrice = extractNumberFromElement(todayElement);
 
-                // Î≥ÄÎèôÌè≠ Î∞è Î≥ÄÎèôÎ•† Í∞ÄÏ†∏Ïò§Í∏∞
+                // ∫Øµø∆¯ π◊ ∫Øµø∑¸ ∞°¡Æø¿±‚
                 Element exdayElement = doc.selectFirst(".no_exday");
                 String priceChange = "";
                 String changePercentage = "";
                 
-                // 52Ï£º ÏµúÍ≥†/ÏµúÏ†Ä Í∞ÄÍ≤©
-                Element weekHighElement = doc.selectFirst(".no_info td:contains(52Ï£º ÏµúÍ≥†) em");
-                Element weekLowElement = doc.selectFirst(".no_info td:contains(52Ï£º ÏµúÏ†Ä) em");
+                // 52¡÷ √÷∞Ì/√÷¿˙ ∫Ò±≥
+                Element weekHighElement = doc.selectFirst(".no_info td:contains(52¡÷ √÷∞Ì) em");
+                Element weekLowElement = doc.selectFirst(".no_info td:contains(52¡÷ √÷¿˙) em");
 
                 String weekHigh52 = extractNumberFromElement(weekHighElement);
                 String weekLow52 = extractNumberFromElement(weekLowElement);
@@ -103,21 +103,19 @@ public class StockInterCrawler {
                     }
                 }
                 
-                
-
-                // Í∏∞Î≥∏Í∞í ÏÑ§Ï†ï Î∞è Í∞íÏù¥ ÎπÑÏñ¥ ÏûàÏùÑ Í≤ΩÏö∞ Í≤ÄÏ¶ù
+                // ±‚∫ª∞™ º≥¡§ π◊ ∞™¿Ã ∫ÒæÓ ¿÷¿ª ∞ÊøÏ ¥Î∫Ò
                 double currentPriceValue = currentPrice.isEmpty() ? 0.0 : Double.parseDouble(currentPrice.replace(",", ""));
                 double priceChangeValue = priceChange.isEmpty() ? 0.0 : Double.parseDouble(priceChange.replace(",", ""));
                 double changePercentageValue = changePercentage.isEmpty() ? 0.0 : Double.parseDouble(changePercentage);
                 double weekHigh52Value = weekHigh52.isEmpty() ? 0.0 : Double.parseDouble(weekHigh52.replace(",", ""));
                 double weekLow52Value = weekLow52.isEmpty() ? 0.0 : Double.parseDouble(weekLow52.replace(",", ""));
 
-                // changePercentageÍ∞Ä ÏùåÏàòÏù∏ Í≤ΩÏö∞ priceChangeÎèÑ ÏùåÏàòÎ°ú ÏÑ§Ï†ï
+                // changePercentage∞° ¿Ωºˆ¿Œ ∞ÊøÏ priceChangeµµ ¿Ωºˆ∑Œ º≥¡§
                 if (changePercentageValue < 0) {
                     priceChangeValue = -Math.abs(priceChangeValue);
                 }
 
-                // DTOÏóê Í∞í ÏÑ§Ï†ï
+                // DTOø° ∞™ º≥¡§
                 StockDTO stockDTO = new StockDTO();
                 stockDTO.setCountry(country);
                 stockDTO.setIndexName(indexName);
