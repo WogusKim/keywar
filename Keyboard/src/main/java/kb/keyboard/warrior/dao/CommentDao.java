2c57769e8c3fe3660929f69744510a815779e7d0
KJH <wogus6267@gmail.com>
2024-08-28 16:14:52 +0900

    마이페이지 댓글 역순

--- Keyboard/src/main/java/kb/keyboard/warrior/dao/CommentDao.java
+++ Keyboard/src/main/java/kb/keyboard/warrior/dao/CommentDao.java
@@ -9,6 +9,7 @@ public interface CommentDao {
 	public void addComment(CommentDTO dto) ;
 	public void addLike(LikeDTO dto) ;
 	public List<CommentDTO> getComment(String targerno);
+	public List<CommentDTO> getCommentReverse(String targerno);
 	public int checkLike(LikeDTO dto);
 	public void commentDelete(String commentid);
 	public String findWhoWrote(String commentid);
